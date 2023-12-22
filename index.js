8489251abd7a2ecaef5e04e7d45f16e7ca298bb1
Full Stack Shannon, LLC <fullstackshannon@gmail.com>
2023-12-22 01:22:05 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+﻿import initScrollReveal from "./scripts/scrollReveal";
+import initTiltEffect from "./scripts/tiltAnimation";
+import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+
+initScrollReveal(targetElements, defaultProps);
+initTiltEffect();
