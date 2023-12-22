e86dc8e683cc54a6480e5213ad8f604b96b4873a
Shannon P White <fullstackshannon@gmail.com>
2023-12-22 08:53:40 -0800

    commit
    Former-commit-id: 7a0179a5e490fdecbad03f3a07e958008bcead5a

--- _site/src/index.js
+++
@@ -1,6 +0,0 @@
-﻿import initScrollReveal from "./scripts/scrollReveal";
-import initTiltEffect from "./scripts/tiltAnimation";
-import { targetElements, defaultProps } from "./data/scrollRevealConfig";
-
-initScrollReveal(targetElements, defaultProps);
-initTiltEffect();
