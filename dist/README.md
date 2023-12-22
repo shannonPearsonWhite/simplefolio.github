7c39132a837f76d8351862e0a100dc89710c45ba
Shannon P White <fullstackshannon@gmail.com>
2023-12-22 09:40:38 -0800

    build
    Former-commit-id: ad8435ccb1be179d45ee6e8abc84ba9941c0be9e

---
+++ dist/README.md
@@ -0,0 +1,9 @@
+---
+layout: post
+title: Blogging Like a Hacker
+---
+
+
+
+Created by FullStackShannon
+To contact me email at : fullstackshannon.gmail.com
---
+++ dist/index.js
@@ -0,0 +1,13 @@
+import initScrollReveal from "./scripts/scrollReveal";
+import initTiltEffect from "./scripts/tiltAnimation";
+import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+
+initScrollReveal(targetElements, defaultProps);
+initTiltEffect();
+
+import initScrollReveal from "./scripts/scrollReveal";
+import initTiltEffect from "./scripts/tiltAnimation";
+import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+
+initScrollReveal(targetElements, defaultProps);
+initTiltEffect();
